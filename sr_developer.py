d58b8be41530a7e0f2038b44ca9d122a3ec36d46
Jhoney <51183760+jhoney787813@users.noreply.github.com>
2025-02-21 16:58:50 -0500

    Update sr_developer.py

--- sr_developer.py
+++ sr_developer.py
@@ -1,1 +1,8 @@
-
+ def calculate_average(numbers):
+      if not numbers:
+          return 0 
+      return sum(numbers) / len(numbers)
+  
+  grades = [5.5, 5.8, 4.0, 5.0, 9.2]
+  average = calculate_average(grades)
+  print(f"The average grade is: {average}")
